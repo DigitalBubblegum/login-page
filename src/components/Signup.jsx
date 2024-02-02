5462f2ecd0d722aa911885d432faaf843f428a60
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-02-02 16:22:21 +0530

    -added skeleton signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,20 @@
 const Signup = () =>{
     return(
-        <>
-        Hello Signup
-        </>
+        <div>
+            <h2>Signup</h2>
+            <form>
+                <div>
+                    email: <input />
+                </div>
+                <div>
+                    username: <input />
+                </div>
+                <div>
+                    password: <input />
+                </div>
+                <button type="submit">signup</button>
+            </form>
+        </div>
     )
 }
 export default Signup
